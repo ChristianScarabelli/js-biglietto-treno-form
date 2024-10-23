e46fc4b948da6f0898fe9cae089441d7cdaee829
ChristianScarabelli <scachmabol@hotmail.it>
2024-10-23 16:34:49 +0200

    ADD: generate price and abort buttons

--- app/script.js
+++ app/script.js
@@ -32,7 +32,26 @@ function calculateTicketPrice (numkm, age) {
 }
 
 // Esempio di chiamata alla funzione
-const ticketPrice = calculateTicketPrice(50, 70)
-console.log(ticketPrice)
+// const ticketPrice = calculateTicketPrice(50, 70)
+// console.log(ticketPrice)
 
 
+// Richiamo il bottone per generare il prezzo
+const generatePriceButton = document.getElementById('input_price')
+
+// Definisco gli eventi al click del bottone che genera il prezzo
+generatePriceButton.addEventListener('click', function() {
+    console.log('ho premuto il bottone')
+})
+
+
+
+
+
+// Richiamo il bottone annullare il calcolo del prezzo
+const abortPriceButton = document.getElementById('input_abort')
+
+// Definisco gli eventi al click del bottone che annulla il form
+abortPriceButton.addEventListener('click', function() {
+    console.log('ho premuto il bottone')
+})
